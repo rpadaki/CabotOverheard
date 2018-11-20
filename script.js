4e788b04b23c79266db1e8ce72bece9607199543
Roshan Padaki <roshanpadaki@gmail.com>
2018-11-19 19:48:50 -0500

    Thingy

--- script.js
+++ script.js
@@ -20,3 +20,28 @@ function fetchQuotes(set = setQuotes) {
     xhttp.open("GET", "https://cors-anywhere.herokuapp.com/https://drive.google.com/uc?export=download&id=1buTZD3t9f_hpQyXN1q4EntdEdXuoAToO");
     xhttp.send();
 }
+
+function setEntry(entry, data) {
+    entry.getElementsByClassName("quote")[0].innerHTML = data.quote;
+    entry.getElementsByClassName("attribution")[0].innerHTML = "--" + data.attribution;
+    entry.getElementsByClassName("time")[0].innerHTML = new Date(Date.parse(data.time)).toLocaleString("en-us", {weekday: "long", month: "long", day: "numeric", hour: "numeric", minute:"2-digit"});
+    entry.hash = JSON.stringify(data);
+}
+
+function updateEntry(number) {
+    var entries = Array.from(document.getElementsByClassName("entry"));
+    var displayed = entries.map(e => e.hash);
+    var quotes = getQuotes();
+    var updates = quotes.filter(q => !displayed.includes(JSON.stringify(q)));
+    var update = updates[Math.floor(Math.random()*updates.length)];
+    if (update) {
+        setEntry(entries[number], update);
+    }
+}
+
+fetchQuotes();
+setInterval(function() {fetchQuotes();}, 10000);
+setInterval(function() {
+    num = Math.floor(Math.random() * 4);
+    updateEntry(num);
+}, 10000);
